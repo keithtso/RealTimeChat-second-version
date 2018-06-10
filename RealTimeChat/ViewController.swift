0e04e42acc146ddd15a018a1a6cc01199e2ee8e6
Keith Cao <caoyonghao@hotmail.co.nz>
2018-06-10 12:20:47 +1200

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Keith Cao on 10/06/18.
+//  Copyright © 2018 Keith Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
